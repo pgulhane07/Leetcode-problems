8642c81f436a8a9a2df1a1fc2b3eab017da8aacc
pgulhane07 <pgulhane@buffalo.edu>
2026-01-10 10:46:31 -0500

    Time: 23 ms (34.43%) | Memory: 46.6 MB (27.72%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -33,11 +33,51 @@ class Solution {
 
     }
     public int minimumDeleteSum(String s1, String s2) {
-        int[][] dp= new int[s1.length()][s2.length()];
-        for(int i=0;i<s1.length();i++){
-            Arrays.fill(dp[i], -1);
+        int[][] dp= new int[s1.length()+1][s2.length()+1];
+        // for(int i=0;i<s1.length();i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return rec(dp, s1,s2, 0,0);
+        // int p=1, sadd=0;
+        // for(char c:s2.toCharArray()){
+        //     //if(s1.charAt(p-1)!=s2.charAt(0))
+        //     sadd+= (int) s2.charAt(p-1);
+        //     dp[p][0]= sadd;
+        //     p++;
+        // }
+        for(int i=1;i<=s1.length();i++){
+            dp[i][0]= s1.charAt(i-1) + dp[i-1][0];
         }
-        return rec(dp, s1,s2, 0,0);
+        for(int i=1;i<=s2.length();i++){
+            dp[0][i]= s2.charAt(i-1) + dp[0][i-1];
+        }
+
+        // int q=1, sadd1=0;
+        // for(char c:s1.toCharArray()){
+        //     //if(s1.charAt(0)!=s2.charAt(q))
+        //     sadd1+= (int) s1.charAt(q-1);
+        //     dp[q][0]= sadd1;
+        //     q++;
+        // }
+
+        for(int i=1;i<=s1.length();i++){
+            for(int j=1;j<=s2.length();j++){
+                // int a= Integer.MAX_VALUE;
+                // int min = Integer.MAX_VALUE;
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.min(s1.charAt(i-1)+dp[i-1][j] ,s2.charAt(j-1)+dp[i][j-1] );
+                }
+                // int min1= Math.min((int)s1.charAt(i)+dp[i+1][j] ,(int)s2.charAt(j)+dp[i][j+1]);
+                // dp[i][j] = Math.min(min,min1);
+            }
+        }
+        return dp[s1.length()][s2.length()];
+
+
+
        
         // min(dp(i+1,j+1), char of j + dp[i,j+1] , char i+dp[i+1,j])
        
