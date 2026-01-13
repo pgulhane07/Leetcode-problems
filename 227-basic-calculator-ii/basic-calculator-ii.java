8a9b61d00275535529ce3fb34fb23227e1af07ee
pgulhane07 <pgulhane@buffalo.edu>
2026-01-13 16:38:34 -0500

    Time: 14 ms (85.81%) | Memory: 46.4 MB (89.31%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -4,6 +4,8 @@ class Solution {
         //Stack<Character> ch= new Stack<>();
         int n=0;
         char op='+';
+        int res=0;
+        int last=0;
         for(int i=0;i<s.length();i++){
             char c= s.charAt(i);
             //if(Character.isWhitespace(c) )continue;
@@ -14,32 +16,37 @@ class Solution {
             if(!Character.isDigit(c) && !Character.isWhitespace(c) || i==s.length()-1){
 
                 if(op == '-'){
-                    num.push(n*-1);
+                    res+= last;
+                    last= -n;
+                    //num.push(n*-1);
                 }
                 else if(op=='+'){
-                    num.push(n);
+                    res+= last;
+                    //num.push(n);
+                    last=n;
                 }
                 else if(op=='*'){
-                    int prev= num.pop();
-                    num.push(prev* n);
+                    // int prev= num.pop();
+                    // num.push(prev* n);
+                    last= last * n;
                 }
                 else if(op=='/'){
-                    int prev= num.pop();
-                    num.push(prev/n);
+                    // int prev= num.pop();
+                    // num.push(prev/n);
+                    last= last/n;
                 }
                 op=c;
-                
                 n=0;
                  // if(c == '*')ch.push(c);
             }
         }
-        
+        res+= last;
                 
 
-        int res=0;
-        while(!num.isEmpty()){
-            res+= num.pop();
-        }
+        // int res=0;
+        // while(!num.isEmpty()){
+        //     res+= num.pop();
+        // }
         return res;
 
     }
