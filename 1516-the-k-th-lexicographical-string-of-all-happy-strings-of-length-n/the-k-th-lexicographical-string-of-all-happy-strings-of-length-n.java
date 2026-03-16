8c4761173b59e6d7a3bfcba1744e7eb2b7c12603
Piyush Gulhane <piyushgulhane.ms@gmail.com>
2026-03-16 09:28:52 -0400

    Time: 32 ms (7.33%) | Memory: 47 MB (23.61%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,26 +1,26 @@
 class Solution { 
     List<Character> list;
     List<String> res;
-    public void backtrack(int len, int index, List<Character> arr){
+    public void backtrack(int len, int index, StringBuilder sb){
 
         if(index == len){
-            String s= arr.stream().map(String::valueOf).collect(Collectors.joining());
-            res.add(s);
+            // String s= arr.stream().map(String::valueOf).collect(Collectors.joining());
+            res.add(sb.toString());
             return;
         }
 
         for(int i=0;i<list.size();i++){
             if(index == 0){
-                arr.add(list.get(i));
-                backtrack(len, index+1, arr);
-                arr.remove(arr.size()-1);
+                sb.append(list.get(i));
+                backtrack(len, index+1, sb);
+                sb.setLength(sb.length()-1);
             }
             else{
                 char cur=list.get(i);
-                if(cur == arr.get(arr.size()-1))continue;
-                arr.add(list.get(i));
-                backtrack(len, index+1, arr);
-                arr.remove(arr.size()-1);
+                if(cur == sb.toString().charAt(sb.length()-1))continue;
+                sb.append(list.get(i));
+                backtrack(len, index+1, sb);
+                sb.setLength(sb.length()-1);
             }
         }
 
@@ -28,7 +28,8 @@ class Solution {
     public String getHappyString(int n, int k) {
         list= List.of('a','b','c');
         res= new ArrayList<>();
-        backtrack(n,0, new ArrayList<>());
+        StringBuilder sb= new StringBuilder();
+        backtrack(n,0, sb);
         //System.out.println(res);
         if(k > res.size())return "";
         Collections.sort(res);
