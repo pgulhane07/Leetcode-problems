448fa72df25669bec37acb0ab4dac09169d24979
Piyush Gulhane <piyushgulhane.ms@gmail.com>
2026-05-18 08:30:01 -0400

    Time: 102 ms (5.14%) | Memory: 48.5 MB (85.74%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visited= new boolean[arr.length];
+
+        Deque<Integer> dq= new ArrayDeque<>();
+        dq.add(start);
+        System.out.println(dq);
+        while(!dq.isEmpty()){
+            int x= dq.poll();
+            if(arr[x]==0)return true;
+            if(x+arr[x] < arr.length && !visited[x+arr[x]]){
+                visited[x+arr[x]]= true;
+                dq.add(x+arr[x]);
+            }
+            if(x-arr[x] >=0  && !visited[x-arr[x]]){
+                visited[x-arr[x]]= true;
+                dq.add(x-arr[x]);
+            }
+            System.out.println(dq);
+        }
+        return false;
+    }
+}
