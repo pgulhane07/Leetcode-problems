578147e01ffcafbdbd9952a71fc1f85058890f95
Piyush Gulhane <piyushgulhane.ms@gmail.com>
2026-06-06 11:48:43 -0400

    Time: 2 ms (98.84%) | Memory: 46.6 MB (29.54%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -5,9 +5,9 @@ class Solution {
         for(int i=nums.length-2;i>=0;i--){
             right[i] = right[i+1]+nums[i+1];
         }
-        for(int n:right){
-            System.out.print(n+" ");
-        }
+        // for(int n:right){
+        //     System.out.print(n+" ");
+        // }
         int left=0;
         for(int i=0;i<nums.length;i++){
             res[i] = Math.abs(left - right[i]);
