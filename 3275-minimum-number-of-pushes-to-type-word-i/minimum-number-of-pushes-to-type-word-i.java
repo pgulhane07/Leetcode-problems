3fb1a2239a79995b0e0b09bca548f1078f59aa95
Piyush Gulhane <piyushgulhane.ms@gmail.com>
2026-07-30 10:56:57 -0400

    Time: 2 ms (21.05%) | Memory: 43.7 MB (5.50%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -13,22 +13,37 @@ class Solution {
             map.put(c, map.getOrDefault(c,0)+1);
         }
 
-        if(map.size() < 8)return word.length();
-
-        PriorityQueue<Node> pq= new PriorityQueue<>((a,b)-> b.val- a.val);
-        for(char c :map.keySet()){
-            pq.add(new Node(c, map.get(c)));
+        if(map.size() <= 8)return word.length();
+        else if(map.size() >8 && map.size() <=16 ){
+            int res = 8 + ((map.size()-8)*2);
+            return res;
         }
-        int i=0, mul=1;
-        int res = 0;
-        while(!pq.isEmpty()){
-
-            Node x = pq.poll();
-            res += (x.val * mul);
-            i++;
-            if(i% 8 ==0)mul++;
+        else if(map.size() >16 && map.size() <=24){
+            int res = 8 + 16;
+            res += ((map.size()-16)*3);
+            return res;
         }
-        return res;
+        else{
+            int res = 8 + 16 + 24;
+            res += ((map.size()-24)*4);
+            
+            return res;
+        }
+
+        // PriorityQueue<Node> pq= new PriorityQueue<>((a,b)-> b.val- a.val);
+        // for(char c :map.keySet()){
+        //     pq.add(new Node(c, map.get(c)));
+        // }
+        // int i=0, mul=1;
+        // int res = 0;
+        // while(!pq.isEmpty()){
+
+        //     Node x = pq.poll();
+        //     res += (x.val * mul);
+        //     i++;
+        //     if(i% 8 ==0)mul++;
+        // }
+        // return res;
 
     }
 }
